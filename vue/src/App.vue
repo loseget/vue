c429ca539c621a7b9fb214d7e69ef0702f4c19b3
rossyao <178949233@qq.com>
2017-04-05 17:03:04 +0800

    add element ui

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,50 +1,74 @@
 <template>
   <div id="app">
+    <div>
     <img src="./assets/logo.png">
     <h1 v-text="title"></h1>
     <ul>
-      <li v-for="item in items" v-bind:class="{finished:item.isFinished}" @click="toggleFinish(item)">
+      <li v-for="item in items"
+          v-bind:class="{finished:item.isFinished}"
+          @click="toggleFinish(item)">
         {{item.lable}}
       </li>
     </ul>
+    </div>
+    <div>
+      <el-button @click="visible = true"  type="info">按钮</el-button>
+      <el-dialog v-model="visible"
+                 title="Hello world">
+        <p>欢迎使用 Element</p>
+      </el-dialog>
+      <el-alert title="成功提示的文案"
+                type="success">
+      </el-alert>
+      <el-alert title="消息提示的文案"
+                type="info">
+      </el-alert>
+      <el-alert title="警告提示的文案"
+                type="warning">
+      </el-alert>
+      <el-alert title="错误提示的文案"
+                type="error">
+      </el-alert>
+    </div>
   </div>
 </template>
 
 <script>
-  import Hello from './components/Hello'
-  //什么时候
-  export default {
-    data: function() {
-      return {
-        title: 'it is my life!',
-        items: [{
-            lable: '开始制作游戏',
-            isFinished: false
-          },
-  
-          {
-            lable: '开始弹弹Ukelele',
-            isFinished: false
-          },
-  
-          {
-            lable: '开始学习心理学',
-            isFinished: false
-          }
-  
-        ]
+import Hello from './components/Hello'
+//什么时候
+export default {
+  data: function () {
+    return {
+      title: 'it is my life!',
+      visible :false,
+      items: [{
+        lable: '开始制作游戏',
+        isFinished: false
+      },
+
+      {
+        lable: '开始弹弹Ukelele',
+        isFinished: false
+      },
+
+      {
+        lable: '开始学习心理学',
+        isFinished: false
       }
-    },
-    methods: {
-  
-      toggleFinish: function(item) {
-        console.log(item)
-      }
+
+      ]
+    }
+  },
+  methods: {
+
+    toggleFinish: function (item) {
+      console.log(item)
     }
   }
+}
 </script>
 
-<style>
+<style lang="stylus" rel="stylesheet/stylus">
   #app {
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
